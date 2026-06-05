e96d6e78d83a21dae7460b6b09fde4260114b97a
kongsophanha98 <Kongsophanha096@gmail.com>
2026-06-05 10:26:24 +0700

    Fix conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,7 @@ import JoinGame from "./pages/JoinGame";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import HostDashboard from "./pages/Dashboard";
-<<<<<<< Updated upstream
-import CreateQUiz from "./pages/CreateQuiz";
-=======
-import CreateQuiz from "./pages/CreateQuiz";
->>>>>>> Stashed changes
+import QuizBuilder from "./pages/Quizbuilder";
 
 export default function App() {
   return (
@@ -25,11 +21,8 @@ export default function App() {
         <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<HostDashboard />} />
-<<<<<<< Updated upstream
-        <Route path="/create-quiz" element={<CreateQUiz />} />
-=======
-        <Route path="/create-quiz" element={<CreateQuiz />} />
->>>>>>> Stashed changes
+        <Route path="/host" element={<HostDashboard />} />
+        <Route path="/create-quiz" element={<QuizBuilder />} />
       </Routes>
     </BrowserRouter>
   );
