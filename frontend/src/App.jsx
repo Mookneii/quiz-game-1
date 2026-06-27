d889c2b501d5921ee0a8d88bea99277afed30ab2
Mony <monyjk576@gmail.com>
2026-06-27 23:00:20 +0700

    delete history page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,6 @@ import WaitingAnswer from "./pages/WaitingAnswer";
 import HostLiveGame from "./pages/HostLiveGame";
 import CreateQuiz from "./pages/CreateQuiz";
 import EditQuiz from "./pages/EditQuiz";
-import GameHistory from "./pages/GameHistory";
-import GameHistoryDetail from "./pages/GameHistoryDetail";
 
 export default function App() {
   return (
@@ -45,8 +43,6 @@ export default function App() {
         <Route path="/create-quiz/:quizId" element={<CreateQuiz />} />
         <Route path="/edit-quiz/:quizId" element={<EditQuiz />} />
         <Route path="/join/:roomCode" element={<JoinGame />} />
-        <Route path="/game-history" element={<GameHistory />} />
-        <Route path="/game-history/:gameResultId" element={<GameHistoryDetail />} />
       </Routes>
     </BrowserRouter>
   );
